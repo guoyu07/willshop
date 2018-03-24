1cacbf964bd23beb4ca6d3b81736f2e7713c77e2
tianyong90 <412039588@qq.com>
2018-03-25 00:52:52 +0800

    admin

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-03-12 20:10:51.
+    * Generated on 2018-03-25 00:40:24.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
